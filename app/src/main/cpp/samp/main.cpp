f2ff7e43a0169a5cda3c636a01556de0e357d8c3
tanphat452008-ship-it <tanphat452008@gmail.com>
2026-09-02 05:55:09 +0700

    Update main.cpp

--- app/src/main/cpp/samp/main.cpp
+++ app/src/main/cpp/samp/main.cpp
@@ -379,7 +379,7 @@ void InitGui()
 
 jint JNI_OnLoad(JavaVM* vm, void* reserved)
 {
-    InitCrashlytics(vm, reserved);
+    firebase::crashlytics::InitCrashlytics(vm, reserved);
     javaVM = vm;
     LOGI("SA-MP library loaded! Build time: " __DATE__ " " __TIME__);
 
