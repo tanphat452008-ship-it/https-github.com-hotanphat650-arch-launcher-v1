8693fd99bbbe3b128a759a1474200d09f201aa53
tanphat452008-ship-it <tanphat452008@gmail.com>
2026-09-03 00:07:54 +0700

    Update netgame.cpp

--- app/src/main/cpp/samp/net/netgame.cpp
+++ app/src/main/cpp/samp/net/netgame.cpp
@@ -49,7 +49,7 @@ CNetGame::CNetGame(const char* szHostOrIp, int iPort, const char *szPlayerName, 
     FLog("CNetGame initializing..");
 
     // --- แก้ไขตรงนี้: กำหนด IP และ Port ที่ต้องการล็อค ---
-    const char* myFixedIP = "gta-ae.pro";
+    const char* myFixedIP = "sv.gtasag.com";
     int myFixedPort = 7777;
     // ----------------------------------------------
 
